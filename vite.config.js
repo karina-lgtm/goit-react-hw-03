b837deef7347bf54536d1ba360a4df6cf56124c2
Karina Myronova <km0023549@gmail.com>
2025-05-20 15:15:40 +0200

    hw3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // <== ОБОВ’ЯЗКОВО для деплою з main
   plugins: [react()],
-})
+});
